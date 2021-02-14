5e42e891cd87f02843a6bc9ebbcef18a9d4e96c3
albert239825 <albert23982@gmail.com>
2021-02-14 12:02:03 -0500

    Adding the cnn_model_32, I might of messed up before

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,29 +6,21 @@ from tensorflow.keras.models import load_model
 import os
 import math
 
-#convert the file into wav format
-def convertToWav(input_audio):
-    sound = AudioSegment.from_mp3(input_audio)
-    if (len(sound) > 29000):
-        print("success")
-    else:   
-        print("short file")
-    song = sound.export(format = "wav")
-    return song
 
 #extract MFCCs
-def extract_mfcc(signal):
+def extract_mfcc(signal, SAMPLE_RATE):
     #variable declaration
     n_mfcc = 32
     n_fft = 2048
     hop_length = 512
 
-    DURATION = len(signal) / SAMPLE_RATE
-    SAMPLES_PER_TRACK = len(signal)
     #divide the song into 3 second segments so that we can feed into the model. Since it was trained on (129,32) data
-    #segment can variable length??? Look into this...
-    num_segments = (int) (len(signal) / (2.98 * SAMPLE_RATE))
-    num_sam = int(SAMPLES_PER_TRACK / num_segments)
+    #Solving this my having fixed segment length. At most missing 3 seconds of the total song
+    len_segment = (2.98 * SAMPLE_RATE)
+    num_segments = (int) (len(signal) / len_segment)
+    print("len_segment: {}, num_segments {}, cropped signal Length: {}, Original signal length: {}".format(len_segment, num_segments, (len_segment*num_segments), len(signal)))
+    singal_cropped = signal[: (int) (len_segment*num_segments)]
+    num_sam = int(len(singal_cropped) / num_segments)
 
     expected_num_mfcc_vectors_per_segment = math.ceil(num_sam / hop_length) #round up
     print(expected_num_mfcc_vectors_per_segment)
@@ -55,6 +47,8 @@ def extract_mfcc(signal):
 
 
 
+
+
 # st.write("""
 
 # ## Audio Genre Classification
@@ -75,15 +69,8 @@ test_file = "Data/fma_small/000/000002.mp3"
 SAMPLE_RATE = 22050
 signal, sr = librosa.load(test_file, sr = SAMPLE_RATE)
 
-data = extract_mfcc(signal)
+data = extract_mfcc(signal, SAMPLE_RATE)
 
 
-
-
-
-
-
-
-
-# model = load_model('models/resnet50_pretrained_32.h5')
+model = load_model('models/cnn_model_32.h5')
 
