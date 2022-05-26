d4127c73c7ec023edf109c115c39517d56d7ee2e
albert239825 <albert23982@gmail.com>
2022-05-26 16:15:53 -0400

    Working on converting the model into the ONNX format

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,24 @@ import numpy as np
 import pickle
 
 
-#extract MFCCs
+# extract MFCCs
 def extract_mfcc(signal, SAMPLE_RATE):
-    #variable declaration
+    # variable declaration
     n_mfcc = 32
     n_fft = 2048
     hop_length = 512
 
-    #divide the song into 3 second segments so that we can feed into the model. Since it was trained on (129,32) data
-    #Solving this my having fixed segment length. At most missing 3 seconds of the total song
+    # divide the song into 3 second segments so that we can feed into the model. Since it was trained on (129,32) data
+    # Solving this my having fixed segment length. At most missing 3 seconds of the total song
     len_segment = (2.98 * SAMPLE_RATE)
-    num_segments = (int) (len(signal) / len_segment)
-    print("len_segment: {}, num_segments {}, cropped signal Length: {}, Original signal length: {}".format(len_segment, num_segments, (len_segment*num_segments), len(signal)))
-    singal_cropped = signal[: (int) (len_segment*num_segments)]
+    num_segments = (int)(len(signal) / len_segment)
+    print("len_segment: {}, num_segments {}, cropped signal Length: {}, Original signal length: {}".format(
+        len_segment, num_segments, (len_segment*num_segments), len(signal)))
+    singal_cropped = signal[: (int)(len_segment*num_segments)]
     num_sam = int(len(singal_cropped) / num_segments)
 
-    expected_num_mfcc_vectors_per_segment = math.ceil(num_sam / hop_length) #round up
+    expected_num_mfcc_vectors_per_segment = math.ceil(
+        num_sam / hop_length)  # round up
     print(expected_num_mfcc_vectors_per_segment)
 
     st.write("processing Audio")
@@ -33,11 +35,11 @@ def extract_mfcc(signal, SAMPLE_RATE):
         start_sample = num_sam * s
         finish_sample = start_sample + num_sam
 
-        mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample], 
-                                    sr = sr,
-                                    n_fft = n_fft,
-                                    n_mfcc = n_mfcc,
-                                    hop_length = hop_length)
+        mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample],
+                                    sr=sr,
+                                    n_fft=n_fft,
+                                    n_mfcc=n_mfcc,
+                                    hop_length=hop_length)
         mfcc = mfcc.T
 
         if len(mfcc) == expected_num_mfcc_vectors_per_segment:
@@ -48,10 +50,6 @@ def extract_mfcc(signal, SAMPLE_RATE):
 
     data = np.array(data)
     return data
-
-
-
-
 
 
 st.write("""
@@ -68,39 +66,39 @@ test_file = "Data/fma_small/000/000002.mp3"
 with open('data/mapping.pickle', 'rb') as f:
     mapping = pickle.load(f)
 
-#getting the file
+# getting the file
 uploaded_file = st.file_uploader("Upload Files", type='.mp3')
 
-#checking that we have an uploaded file
-# if uploaded_file is not None:
-#     #note for later, how might this conflict if there are two people running the same script
-#     filepath = 'temp/' + uploaded_file.name
-#     with open(filepath, "wb") as f:
-#         f.write(uploaded_file.getbuffer())
+# checking that we have an uploaded file
+if uploaded_file is not None:
+    # note for later, how might this conflict if there are two people running the same script
+    filepath = 'temp/' + uploaded_file.name
+    with open(filepath, "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
-#     #display the audio file
-#     st.audio(filepath, format='audio/mp3')
+    # display the audio file
+    st.audio(filepath, format='audio/mp3')
 
-#     #loading in the audiofile
-#     SAMPLE_RATE = 22050
-#     signal, sr = librosa.load(filepath, sr = SAMPLE_RATE)
+    # loading in the audiofile
+    SAMPLE_RATE = 22050
+    signal, sr = librosa.load(filepath, sr=SAMPLE_RATE)
 
-#     data = extract_mfcc(signal, SAMPLE_RATE)
-#     #expanding dimension to feed into CNN
-#     data_cnn = np.expand_dims(data, axis = -1)
+    data = extract_mfcc(signal, SAMPLE_RATE)
+    # expanding dimension to feed into CNN
+    data_cnn = np.expand_dims(data, axis=-1)
 
-#     model = load_model('models/cnn_model_32.h5')
-#     predictions = model.predict(data_cnn)
-#     #used to calculate how sure we are
-#     total_prob = predictions.shape[0]
-#     sum_predictions = predictions.sum(axis = 0)
-#     predicted_indicies = np.argsort(sum_predictions)
+    model = load_model('models/cnn_model_32.h5')
+    predictions = model.predict(data_cnn)
+    # used to calculate how sure we are
+    total_prob = predictions.shape[0]
+    sum_predictions = predictions.sum(axis=0)
+    predicted_indicies = np.argsort(sum_predictions)
 
-#     print(sum_predictions, predicted_indicies, mapping)
-        
-#     #Really Messy Print statement that tells us the prediction certainties
-#     st.write("The first prediction is: {} with a {}% certainty , the second guess is: {} with a {}% certainty"
-#     .format(mapping[predicted_indicies[-1]], round((sum_predictions[predicted_indicies[-1]] / total_prob) * 100, 2),
-#       mapping[predicted_indicies[-2]], round((sum_predictions[predicted_indicies[-2]] / total_prob) * 100, 2)))
+    print(sum_predictions, predicted_indicies, mapping)
 
-#     os.remove(filepath)
+    # Really Messy Print statement that tells us the prediction certainties
+    st.write("The first prediction is: {} with a {}% certainty , the second guess is: {} with a {}% certainty"
+             .format(mapping[predicted_indicies[-1]], round((sum_predictions[predicted_indicies[-1]] / total_prob) * 100, 2),
+                     mapping[predicted_indicies[-2]], round((sum_predictions[predicted_indicies[-2]] / total_prob) * 100, 2)))
+
+    os.remove(filepath)
